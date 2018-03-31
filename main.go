4b06ec915397bd2e2406e3d062e3f055ce741e76
Bigghead <shavenchew@yahoo.com>
2018-03-31 15:47:54 -0700

    copied code to try image-reads

--- main.go
+++ main.go
@@ -1,5 +1,57 @@
 package main
 
-func main() {
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
 
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2/google"
+
+	vision "google.golang.org/api/vision/v1"
+)
+
+func main() {
+	readFile()
+
+}
+
+func readFile() {
+
+	data, err := ioutil.ReadFile("Rottweiler-1.jpg")
+
+	enc := base64.StdEncoding.EncodeToString(data)
+	img := &vision.Image{Content: enc}
+
+	feature := &vision.Feature{
+		Type:       "LABEL_DETECTION",
+		MaxResults: 10,
+	}
+
+	req := &vision.AnnotateImageRequest{
+		Image:    img,
+		Features: []*vision.Feature{feature},
+	}
+
+	batch := &vision.BatchAnnotateImagesRequest{
+		Requests: []*vision.AnnotateImageRequest{req},
+	}
+
+	ctx := context.Background()
+
+	client, err := google.DefaultClient(ctx, vision.CloudPlatformScope)
+
+	svc, err := vision.New(client)
+	if err != nil {
+		log.Fatal(err)
+	}
+	res, err := svc.Images.Annotate(batch).Do()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body, err := json.Marshal(res.Responses[0].LabelAnnotations)
+	fmt.Println(string(body))
 }
